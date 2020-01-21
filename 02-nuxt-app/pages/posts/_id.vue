bb652764f49d35dee00d69d2e3ff819118716eb5
David Dangerfield <ddangerfield@smugmug.com>
2020-01-20 19:47:25 -0600

    add `posts` to demo dynamic routes

---
+++ 02-nuxt-app/pages/posts/_id.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h2>Post Details</h2>
+    <h3>Title</h3>
+    <p>{{ post.title }}</p>
+    <h3>Body</h3>
+    <p>{{ post.body }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  asyncData(context) {
+    return fetch(
+      'https://jsonplaceholder.typicode.com/posts/' + context.params.id
+    )
+      .then(response => response.json())
+      .then(post => ({ post }))
+  }
+}
+</script>
---
+++ 02-nuxt-app/pages/posts/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h2>All Posts</h2>
+    <div v-for="post in posts" :key="post.id">
+      <nuxt-link :to="String(post.id)" append>{{ post.title }}</nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  asyncData(context) {
+    return fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(posts => ({ posts }))
+  }
+}
+</script>
+
+<style></style>
