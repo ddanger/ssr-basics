8c57cb1b0adf8ab13746c58fb68367e2c0d871e8
David Dangerfield <ddangerfield@smugmug.com>
2020-01-20 19:40:13 -0600

    add directory to demo `asyncData`

---
+++ 02-nuxt-app/pages/directory.vue
@@ -0,0 +1,31 @@
+<template>
+  <main>
+    <h2>Directory</h2>
+    <p>These are the imaginary people who use this goofy app.</p>
+
+    <table>
+      <thead>
+        <tr>
+          <th>username</th>
+          <th>email</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="user in users" :key="user.id">
+          <td>{{ user.username }}</td>
+          <td>{{ user.email }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </main>
+</template>
+
+<script>
+export default {
+  asyncData(context) {
+    return fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => ({ users }))
+  }
+}
+</script>
