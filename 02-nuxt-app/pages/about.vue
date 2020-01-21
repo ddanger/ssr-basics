fa9deb92247ba021594cfbe220e16f585f5c6534
David Dangerfield <ddangerfield@smugmug.com>
2020-01-20 19:38:51 -0600

    add about page

---
+++ 02-nuxt-app/pages/about.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="about">
+    <h2>About</h2>
+    <p>
+      This is an "about" page for this app to tell you about it so you don't
+      have to shrug in confusion.
+    </p>
+  </div>
+</template>
